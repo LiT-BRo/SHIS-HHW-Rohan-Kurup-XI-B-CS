6eb9f8e39d034b669770994b9318ad1d306f7597
LiT-BRo <55909475+LiT-BRo@users.noreply.github.com>
2021-06-16 00:44:21 +0530

    Added Project-9

---
+++ Custom-Projects/Project-9/Number-Composite-Prime.py
@@ -0,0 +1,26 @@
+print("\n\n====== Welcome to Ro's Number Detector v0.2 ======\n")
+notice = "\n\tNOTICE: To exit press E"
+
+while True:
+    def program():
+        for i in range(2):
+            number = input("\nEnter a number => ")
+
+            if number.lower() == 'e':
+                print("====== Thanks for using Ro's Number Detector v0.2 ======")
+                exit()
+
+            elif int(number) <= 1:
+                print(f"\nThe number {number} is NITHER PRIME NOR COMPOSITE")
+
+            elif int(number) > 1:
+                for i in range(2, (int(number))): #So the Divisor(i) is ranging from 2 to the Inputed Number.
+                    if int(number) % i == 0: #If number is divisibe by any of the numbers in range.
+                        print(f"\nThe number {number} is a COMPOSITE NUMBER")
+                        break
+                else: 
+                    print(f"\nThe number {number} is a PRIME NUMBER")
+
+        print(notice)
+        program()
+    program()
