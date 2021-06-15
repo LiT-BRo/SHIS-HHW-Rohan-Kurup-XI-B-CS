908ca5a33bf341fdcc0ee04c46e124854e8bd7c9
LiT-BRo <55909475+LiT-BRo@users.noreply.github.com>
2021-06-16 00:06:21 +0530

    Added Project-8

---
+++ Custom-Projects/Project-8/Leap-Year-Finder.py
@@ -0,0 +1,19 @@
+def year():
+    year = int(input("\nEnter the year: "))
+
+    if (year % 4) == 0:
+        if (year % 100) == 0:
+            if (year % 400) == 0:
+                print(year, "is a leap year")
+            else:
+                print(year, "is not a leap year")
+        else:
+            print(year, "is a leap year")
+    else:
+        print(year, "is not a leap year")
+year()
+
+def progres(): 
+    year()
+    progres()
+progres()
