f031d9192e55e2a7b16c7bccba6a669e2c12aa41
LiT-BRo <55909475+LiT-BRo@users.noreply.github.com>
2021-06-17 11:02:14 +0530

    Added Program-4

---
+++ Main-Project/Program-4/Letter-Pattern.py
@@ -0,0 +1,6 @@
+for i in range(1, 6):
+    a = 65 #Converting the ASCII values to charaters and using this creating the list and the desired pattern.
+    print()
+    for alphabet in range(i):
+        print(chr(a), end="")
+        a += 1
