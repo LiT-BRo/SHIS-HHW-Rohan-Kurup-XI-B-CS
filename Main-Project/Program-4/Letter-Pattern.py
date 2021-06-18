9f65c1554885919f2992b220022e19ee9a260615
LiT-BRo <55909475+LiT-BRo@users.noreply.github.com>
2021-06-18 19:43:52 +0530

    Updated Program-4 [Final]

--- Main-Project/Program-4/Letter-Pattern.py
+++ Main-Project/Program-4/Letter-Pattern.py
@@ -1,6 +1,9 @@
+#This program prints the number pattern as required by the question.
+
 for i in range(1, 6):
     a = 65 #Converting the ASCII values to charaters and using this creating the list and the desired pattern.
     print()
     for alphabet in range(i):
         print(chr(a), end="")
         a += 1
+#-LiTBRo
