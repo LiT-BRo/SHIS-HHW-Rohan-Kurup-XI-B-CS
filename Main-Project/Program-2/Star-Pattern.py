575ba5342ad7b1a10cf753a1418095af10da4ced
LiT-BRo <55909475+LiT-BRo@users.noreply.github.com>
2021-06-18 19:03:23 +0530

    Updated Program-2 [Final]

--- Main-Project/Program-2/Star-Pattern.py
+++ Main-Project/Program-2/Star-Pattern.py
@@ -1,4 +1,8 @@
+#This program calculates the aggegate of Percenatge scored on the input of the marks.
+#Debugged to avoid any faulty inputs.
+
 print("\nPrinting the pattern in a list using a 'for loop':\n ")
 
 for i in range(1, 6):
     print("*"*i)
+#-LiTBRo
