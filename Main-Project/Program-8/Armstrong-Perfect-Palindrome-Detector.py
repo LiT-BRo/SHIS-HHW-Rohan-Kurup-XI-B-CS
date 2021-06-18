a014c479836008b5ab6b6c8ab3f123177822fae0
LiT-BRo <55909475+LiT-BRo@users.noreply.github.com>
2021-06-18 06:09:41 +0530

    Added Program-8

---
+++ Main-Project/Program-8/Armstrong-Perfect-Palindrome-Detector.py
@@ -0,0 +1,49 @@
+def armstrong(user_input):
+    answer = 0
+    units = len(str(user_input)) #Getting the length of the number for exponentiation
+
+    for number in str(user_input): #Targetting each number individually
+        answer += int(number)**units #Exponentiating the number at the active units place with the length of number to get the desired answer.
+    
+    if user_input == answer:
+        return True #Number is Amrstrong
+    else:
+        return False #Number not Amrstrong
+
+def perfect(user_input):
+    answer = 0
+    for divisor in range(1, user_input):
+        if user_input % divisor == 0:
+            answer += divisor
+        else:
+            pass
+    
+    if user_input == answer:
+        return True #Number is perfect
+    else:
+        return False #Number not perfect
+
+def palindrome(user_input):
+    if len(str(user_input)) > 1:
+        answer = int(str(user_input)[::-1])
+
+        if user_input == answer:
+            print(True)
+            return True #Number is palindrome
+    else:
+        return False #Number not palindrome
+
+def main():
+    print("\n\n====== Welcome to Ro's Number Detector v0.1 ========")
+
+    user_input = int(input("\nEnter the number => "))
+
+    if armstrong(user_input) == True: #0, 1, 153, 371, 407, 1634, 8208, 9474, 54748
+        print(f"\nThe number \'{user_input}\' is an Armstrong Number")
+    if perfect(user_input) == True: #6, 28, 496, 8128
+        print(f"\nThe number \'{user_input}\' is a Perfect Number")
+    if palindrome(user_input) == True: #Two digit or more
+        print(f"\nThe number \'{user_input}\' is a Palindrome Number")
+    if armstrong(user_input) == False & perfect(user_input) == False & palindrome(user_input) == False:
+        print(f"\nThe number \'{user_input}\' is an Integer")
+main()
