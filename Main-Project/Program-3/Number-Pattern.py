d8757693523c9dd8b9c23899051f4a185d60a2ac
LiT-BRo <55909475+LiT-BRo@users.noreply.github.com>
2021-06-18 19:13:51 +0530

    Updated Program-3 [Final]

--- Main-Project/Program-3/Number-Pattern.py
+++ Main-Project/Program-3/Number-Pattern.py
@@ -1,6 +1,9 @@
+#This program prints the number pattern as required by the question.
+
 for i in range(5, 0, -1):
     a = 1
     print()
     for number in range(i):
         print(a, end=" ")
         a = a+1
+#-LiTBRo
